912a9437dfb6d2180ab90c94d0c0b54f70ea8c7e
PraxTube <lukinator28@gmail.com>
2024-01-11 14:10:53 +0100

    feat!: Use `warp` instead of `TcpStream` for POST

--- src/leaderboard.rs
+++ src/leaderboard.rs
@@ -1,49 +1,7 @@
 use std::fs::File;
-use std::io::{self, BufRead, Write};
+use std::io::Write;
 
 const FILE_PATH: &str = "/var/www/html/games/insta-kill/leaderboard.csv";
-
-fn get_leaderboard() -> Vec<(String, u32, String, String)> {
-    let file = match File::open(FILE_PATH) {
-        Ok(r) => r,
-        Err(err) => {
-            println!("Error while trying to fetch file, {}", err);
-            return Vec::new();
-        }
-    };
-    let reader = io::BufReader::new(file);
-
-    let mut board: Vec<(String, u32, String, String)> = reader
-        .lines()
-        .filter_map(|line| {
-            line.ok().and_then(|l| {
-                let parts: Vec<&str> = l.split(',').map(str::trim).collect();
-                match parts.as_slice() {
-                    [key, score, kills, time] => score
-                        .parse()
-                        .ok()
-                        .map(|v: u32| (key.to_string(), v, kills.to_string(), time.to_string())),
-                    _ => None,
-                }
-            })
-        })
-        .collect();
-    board.sort_by(|a, b| a.1.cmp(&b.1));
-    board.reverse();
-    board
-}
-
-pub fn leaderboard_http() -> String {
-    let board = get_leaderboard();
-
-    let mut result = String::new();
-
-    for (name, score, kills, time) in board {
-        result.push_str(&format!("{},{},{},{};", name, score, kills, time));
-    }
-
-    result.trim_end_matches(";").to_string()
-}
 
 pub fn add_to_leaderboard(data_line: &str) {
     let mut file = match File::options().append(true).create(true).open(FILE_PATH) {
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,34 @@
 mod leaderboard;
 
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::thread;
+use bytes::Bytes;
+use warp::Filter;
 
-use crate::leaderboard::{add_to_leaderboard, leaderboard_http};
+use crate::leaderboard::add_to_leaderboard;
 
-fn add_leaderboard_response(request: String) -> String {
-    if let Some(start) = request.find("\r\n\r\n") {
-        let body = &request[start + 4..];
-        let lines: Vec<&str> = body.split("\r\n").collect();
-
-        if !lines.is_empty() {
-            let data_line = lines[0];
-            if data_line.chars().filter(|&c| c == ',').count() == 3 {
-                add_to_leaderboard(data_line);
-                return "HTTP/1.1 200 OK\r\n\r\nData received successfully".to_string();
-            }
-        }
+fn add_leaderboard_response(body: String) -> String {
+    if body.chars().filter(|&c| c == ',').count() == 3 {
+        add_to_leaderboard(&body);
+        return "HTTP/1.1 200 OK\r\n\r\nData received successfully".to_string();
     }
-    println!("ERROR, POST request doesn't match");
+
+    println!("ERROR, POST request doesn't match, {}", body);
     "HTTP/1.1 400 Bad Request\r\n\r\nGiven leaderboard entry is not correct".to_string()
 }
 
-fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    let request = String::from_utf8_lossy(&buffer[..]);
+#[tokio::main]
+async fn main() {
+    let request = warp::post()
+        .and(warp::path("board"))
+        .and(warp::body::content_length_limit(1024))
+        .and(warp::body::bytes())
+        .map(|bytes: Bytes| {
+            let s = match String::from_utf8(bytes.to_vec()) {
+                Ok(r) => r,
+                Err(_) => return warp::reply(),
+            };
+            add_leaderboard_response(s);
+            warp::reply()
+        });
 
-    let response = if request.starts_with("POST") {
-        add_leaderboard_response(request.to_string())
-    } else if request.starts_with("GET") {
-        leaderboard_http()
-    } else {
-        "HTTP/1.1 400 Bad Request\r\n\r\nOnly POST and GET requests are supported".to_string()
-    };
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
-
-fn main() {
-    let listener = TcpListener::bind("0.0.0.0:3434").expect("cannot bind to ip address");
-    println!("Server listening on port 3434...");
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(|| {
-                    handle_client(stream);
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
-        }
-    }
+    warp::serve(request).run(([0, 0, 0, 0], 3434)).await
 }
