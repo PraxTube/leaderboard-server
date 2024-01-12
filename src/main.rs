6e14a5c19b655c5500c6d0670f30e6a9422af03a
PraxTube <lukinator28@gmail.com>
2024-01-12 09:46:24 +0100

    fix: Use `f32` for time instead of `String`

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use warp::Filter;
 
 use crate::leaderboard::add_to_leaderboard;
 
-fn add_leaderboard_response(name: String, score: u32, kills: u32, time: String) -> String {
+fn add_leaderboard_response(name: String, score: u32, kills: u32, time: f32) -> String {
     let body = format!("{},{},{},{}", name, score, kills, time);
     add_to_leaderboard(&body);
     "HTTP/1.1 200 OK\r\n\r\nData received successfully".to_string()
@@ -17,7 +17,7 @@ async fn main() {
         .and(warp::path::param::<String>())
         .and(warp::path::param::<u32>())
         .and(warp::path::param::<u32>())
-        .and(warp::path::param::<String>())
+        .and(warp::path::param::<f32>())
         .map(|name, score, kills, time| {
             add_leaderboard_response(name, score, kills, time);
             warp::reply()
