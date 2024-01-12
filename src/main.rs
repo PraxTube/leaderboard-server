ce03927e6efef9ee55c0ddd63049c5a765525041
PraxTube <lukinator28@gmail.com>
2024-01-12 09:22:17 +0100

    restructure!: Use URL to pass parameters

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,25 @@
 mod leaderboard;
 
-use bytes::Bytes;
 use warp::Filter;
 
 use crate::leaderboard::add_to_leaderboard;
 
-fn add_leaderboard_response(body: String) -> String {
-    if body.chars().filter(|&c| c == ',').count() == 3 {
-        add_to_leaderboard(&body);
-        return "HTTP/1.1 200 OK\r\n\r\nData received successfully".to_string();
-    }
-
-    println!("ERROR, POST request doesn't match, {}", body);
-    "HTTP/1.1 400 Bad Request\r\n\r\nGiven leaderboard entry is not correct".to_string()
+fn add_leaderboard_response(name: String, score: u32, kills: u32, time: String) -> String {
+    let body = format!("{},{},{},{}", name, score, kills, time);
+    add_to_leaderboard(&body);
+    "HTTP/1.1 200 OK\r\n\r\nData received successfully".to_string()
 }
 
 #[tokio::main]
 async fn main() {
     let request = warp::post()
         .and(warp::path("leaderboard"))
-        .and(warp::body::content_length_limit(1024 * 1024))
-        .and(warp::body::bytes())
-        .map(|bytes: Bytes| {
-            let s = match String::from_utf8(bytes.to_vec()) {
-                Ok(r) => r,
-                Err(_) => return warp::reply(),
-            };
-            add_leaderboard_response(s);
+        .and(warp::path::param::<String>())
+        .and(warp::path::param::<u32>())
+        .and(warp::path::param::<u32>())
+        .and(warp::path::param::<String>())
+        .map(|name, score, kills, time| {
+            add_leaderboard_response(name, score, kills, time);
             warp::reply()
         });
 
